f6a11a965c5211130d125f861956d1a74c3bb1c6
erlhuang <erikkuaisaka@gmail.com>
2021-02-10 14:13:37 -0800

    Initial commit

---
+++ src/main/java/com/revature/ResturantRandomizerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResturantRandomizerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResturantRandomizerBackendApplication.class, args);
+	}
+
+}
