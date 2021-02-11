25eda1557a9ddfa641997cc1e8176dd3b4d61bec
mcdonalddylan <soupaface@gmail.com>
2021-02-11 15:41:47 -0500

    idk bro

--- src/main/java/com/revature/controller/FoodController.java
+++ src/main/java/com/revature/controller/FoodController.java
@@ -5,11 +5,13 @@ import com.revature.repo.FoodRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins= {"http://localhost:3000"})
 @RestController
 @RequestMapping(path="/home")
 public class FoodController {
