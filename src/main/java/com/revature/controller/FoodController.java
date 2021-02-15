c5e005a19493d6ed16afafbc33b4bd59bffdcb62
mcdonalddylan <soupaface@gmail.com>
2021-02-15 11:11:01 -0500

    small changes

--- src/main/java/com/revature/controller/FoodController.java
+++ src/main/java/com/revature/controller/FoodController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins= {"http://localhost:3000"})
+@CrossOrigin
 @RestController
 @RequestMapping(path="/home")
 public class FoodController {
--- src/main/java/com/revature/controller/RestaurantController.java
+++ src/main/java/com/revature/controller/RestaurantController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins= {"http://localhost:3000"})
+@CrossOrigin
 @RestController
 @RequestMapping(path="/goOut")
 public class RestaurantController {
